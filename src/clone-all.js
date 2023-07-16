43e6e828741b32c0b930c198bbb907bbc6645c3e
Utsab Bera <nCoder24@users.noreply.github.com>
2023-07-16 22:16:57 +0530

    update clone all to re clone each of user repos

--- src/clone-all.js
+++ src/clone-all.js
@@ -1,19 +1,20 @@
 const { exec } = require("node:child_process");
+const fs = require("fs");
 
 const clone = ({ username, repos }) =>
-  repos.forEach(({repo, src}) => {
+  repos.forEach(({ repo, src }) => {
     const cloneCmd = `git clone https://github.com/${username}/${repo} ${src}`;
-    exec(cloneCmd, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
+    exec(cloneCmd, (error, stderr) => {
+      if (error || stderr) {
+        console.error(`failed to fetch ${username}/${repo}`);
         return;
       }
-
-      console.log(stdout);
-      console.error(stderr);
     });
   });
 
-const cloneAll = (users) => users.forEach(clone);
+const cloneAll = (users) => {
+  fs.rmSync(".repos", {recursive: true});
+  users.forEach(clone);
+};
 
 module.exports = cloneAll;
